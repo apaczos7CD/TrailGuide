47f0ce32df30cb88eb74ce14f70c65b32a794475
andrzej7CD <andrzej.paczos@student.wat.edu.pl>
2026-06-04 00:17:59 +0200

    Initial of backend structure and files

---
+++ trailguide-backend/src/main/java/pl/trailguide/backend/TrailguideBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.trailguide.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailguideBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailguideBackendApplication.class, args);
+	}
+
+}
---
+++ trailguide-backend/src/test/java/pl/trailguide/backend/TrailguideBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.trailguide.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailguideBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
